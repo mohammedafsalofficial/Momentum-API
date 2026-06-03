673e01103583b2d001330ac573be92cdcf34b16b
Mohammed Afsal <mohammedafsaltut@gmail.com>
2026-06-03 19:24:19 +0530

    refactor: rename test file

--- src/test/java/com/momentum/api/MomentumAPI.java
+++ src/test/java/com/momentum/api/MomentumAPI.java
@@ -2,7 +2,7 @@ package com.momentum.api;
 
 import org.springframework.boot.SpringApplication;
 
-public class MomentumAPIApplication {
+public class MomentumAPI {
 
 	public static void main(String[] args) {
 		SpringApplication.from(MomentumAPI::main).with(TestcontainersConfiguration.class).run(args);

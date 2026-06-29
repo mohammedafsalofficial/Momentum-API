fef8eb0e3430dc782948b0598a6dc5af7e05d5fa
Mohammed Afsal <mohammedafsaltut@gmail.com>
2026-06-29 21:42:42 +0530

    fix: robust password validation using regex

--- src/main/java/com/momentum/api/auth/dto/request/RegisterRequest.java
+++ src/main/java/com/momentum/api/auth/dto/request/RegisterRequest.java
@@ -2,6 +2,7 @@ package com.momentum.api.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,7 +16,11 @@ public class RegisterRequest {
     private String email;
 
     @NotBlank
-    @Size(min = 8, max = 100)
+    @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters long.")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z\\d]).*$",
+            message = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character."
+    )
     private String password;
 
     @NotBlank
